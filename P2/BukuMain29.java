cfa7fa7e691656e564171b23994aab163567c4a9
yonandamayla <yonandamayla555@gmail.com>
2024-02-22 15:54:39 +0700

    add: buku main

---
+++ P2/BukuMain29.java
@@ -0,0 +1,15 @@
+public class BukuMain29 {
+    public static void main(String[] args) {
+        Buku29 bk1 = new Buku29();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+    }
+}
