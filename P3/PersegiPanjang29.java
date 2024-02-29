6b67f752e5dcd1c09912ba8a0c82f7cb3fa18190
yonandamayla <yonandamayla555@gmail.com>
2024-02-29 11:01:19 +0700

    feat : add hitung luas & hitung keliling

--- P3/PersegiPanjang29.java
+++ P3/PersegiPanjang29.java
@@ -2,7 +2,20 @@ public class PersegiPanjang29 {
     public int panjang;
     public int lebar;
 
+    public PersegiPanjang29(int p, int l) {
+        panjang = p;
+        lebar =l;
+    }
+
     public void cetakInfo() {
-        System.out.println("Persegi panjang, panjang : " + panjang + ", lebar : " + lebar);
+        
+    }
+
+    public int hitungLuas() {
+        return panjang * lebar;
+    }
+
+    public int hitungKeliling() {
+        return 2 * (panjang + lebar);
     }
 }
--- P3/PersegiPanjangDemo29.java
+++ P3/PersegiPanjangDemo29.java
@@ -3,20 +3,29 @@ import java.util.Scanner;
 public class PersegiPanjangDemo29 {
     public static void main(String[] args) {
         Scanner sc29 = new Scanner(System.in);
-        PersegiPanjang29[] arrayOfPersegiPanjang29 = new PersegiPanjang29[3];
 
-        for (int i = 0; i < 3; i++) {
-            arrayOfPersegiPanjang29[i] = new PersegiPanjang29();
+        System.out.print("Masukkan jumlah persegi panjang : ");
+        int jmlPersegiPanjang = sc29.nextInt();
+
+        PersegiPanjang29[] arrayOfPersegiPanjang29 = new PersegiPanjang29[jmlPersegiPanjang];
+
+        for (int i = 0; i < jmlPersegiPanjang; i++) {
             System.out.println("Persegi panjang ke - " + (i + 1));
             System.out.print("Masukkan panjang : ");
-            arrayOfPersegiPanjang29[i].panjang = sc29.nextInt();
+            int panjang = sc29.nextInt();
             System.out.print("Masukkan lebar : ");
-            arrayOfPersegiPanjang29[i].lebar = sc29.nextInt();
+            int lebar = sc29.nextInt();
+            arrayOfPersegiPanjang29[i] = new PersegiPanjang29(panjang, lebar);
             System.out.println();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jmlPersegiPanjang; i++) {
             arrayOfPersegiPanjang29[i].cetakInfo();
+            System.out.println("Persegi panjang ke - " + (i + 1) + ". panjang : " + arrayOfPersegiPanjang29[i].panjang
+                    + ", lebar : " + arrayOfPersegiPanjang29[i].lebar + ", luas : "
+                    + arrayOfPersegiPanjang29[i].hitungLuas() + ", keliling : "
+                    + arrayOfPersegiPanjang29[i].hitungKeliling());
         }
+        sc29.close();
     }
 }
