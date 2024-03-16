353dcc162a340612ea3b0bb977bbb76bd767162c
yonandamayla <yonandamayla555@gmail.com>
2024-03-16 20:01:36 +0700

    feat : modif main menu

--- P5/MainPangkat29.java
+++ P5/MainPangkat29.java
@@ -9,21 +9,37 @@ public class MainPangkat29 {
 
         Pangkat29[] png = new Pangkat29[elemen];
         for (int i = 0; i < elemen; i++) {
-            png[i] = new Pangkat29();
             System.out.print("Masukkan nilai yang akan dipangkatkan ke-" + (i + 1) + " : ");
-            png[i].nilai = sc29.nextInt();
+            int nilai = sc29.nextInt();
             System.out.print("Masukkan nilai pemangkat ke-" + (i + 1) + " : ");
-            png[i].pangkat = sc29.nextInt();
+            int pangkat = sc29.nextInt();
+            png[i] = new Pangkat29(nilai, pangkat);
         }
+
         System.out.println("===========================================================");
-        System.out.println("Hasil pangkat dengan brute force : ");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Nilai " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
-        }
-        System.out.println("===========================================================");
-        System.out.println("Hasil pangkat dengan divide conquer : ");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Nilai " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+        System.out.println("Pilih method yang ingin dijalankan:");
+        System.out.println("1. Pangkat dengan Brute Force");
+        System.out.println("2. Pangkat dengan Divide and Conquer");
+        System.out.print("Masukkan pilihan Anda (1/2) : ");
+        int pilihan = sc29.nextInt();
+
+        switch (pilihan) {
+            case 1:
+                System.out.println("===========================================================");
+                System.out.println("Hasil pangkat dengan brute force : ");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Nilai " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            case 2:
+                System.out.println("===========================================================");
+                System.out.println("Hasil pangkat dengan divide conquer : ");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Nilai " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            default:
+                System.out.println("Pilihan tidak valid.");
         }
     }
 }
--- P5/Pangkat29.java
+++ P5/Pangkat29.java
@@ -1,6 +1,12 @@
 public class Pangkat29 {
     public int nilai, pangkat;
 
+    // Konstruktor untuk inisialisasi nilai dan pangkat
+    public Pangkat29(int nilai, int pangkat) {
+        this.nilai = nilai;
+        this.pangkat = pangkat;
+    }
+
     public int pangkatBF(int a, int n) {
         int hasil = 1;
         for (int i = 0; i < n; i++) {
@@ -9,14 +15,14 @@ public class Pangkat29 {
         return hasil;
     }
 
-    public int pangkatDC (int a, int n) {
+    public int pangkatDC(int a, int n) {
         if (n == 0) {
             return 1;
         } else {
-            if (n % 2==1) {//bilangan ganjil
-                return (pangkatDC(a, n/2)*pangkatDC(a, n/2)*a);
+            if (n % 2 == 1) {//bilangan ganjil
+                return (pangkatDC(a, n / 2) * pangkatDC(a, n / 2) * a);
             } else {//bilangan genap
-                return (pangkatDC(a, n/2)*pangkatDC(a, n/2));
+                return (pangkatDC(a, n / 2) * pangkatDC(a, n / 2));
             }
         }
     }
