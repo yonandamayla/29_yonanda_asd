c8e228b0e463ae6980d6ac3fc7b99e38d45c86f6
yonandamayla <yonandamayla555@gmail.com>
2024-03-16 19:07:34 +0700

    feat : hitung waktu nanodetik

--- P5/Faktorial29.java
+++ P5/Faktorial29.java
@@ -1,19 +1,28 @@
 public class Faktorial29 {
     public int nilai;
 
-    public int faktorialBF (int n) {
-        int fakto = 1;
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto * i;
+    public int faktorialBF(int n) {
+        long startTime = System.nanoTime();
+
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            int result = n * faktorialBF(n - 1);
+            long endTime = System.nanoTime();
+            System.out.println("Waktu yang diperlukan untuk faktorialBF(" + n + "): " + (endTime - startTime) + " nanodetik");
+            return result;
         }
-        return fakto;
     }
 
-    public int faktorialDC (int n) {
+    public int faktorialDC(int n) {
+        long startTime = System.nanoTime();
+
         if (n == 1) {
             return 1;
         } else {
-            int fakto = n * faktorialDC(n -1);
+            int fakto = n * faktorialDC(n - 1);
+            long endTime = System.nanoTime();
+            System.out.println("Waktu yang diperlukan untuk faktorialDC(" + n + "): " + (endTime - startTime) + " nanodetik");
             return fakto;
         }
     }
